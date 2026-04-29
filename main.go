3a9e996d1b0262f3e91f29e6ae0b414a241cedd7
Alice GG <hi@alicegg.tech>
2026-04-29 13:37:43 +0200

    Display basic hypno shader

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth  = 512
+	screenHeight = 512
+)
+
+var game *Game
+
+// Game contains the compiled shader and keeps track of the time
+type Game struct {
+	shader    *ebiten.Shader
+	startTime time.Time
+}
+
+// Draw displays the shader on the entire screen
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.DrawRectShader(
+		screenWidth,
+		screenHeight,
+		g.shader,
+		&ebiten.DrawRectShaderOptions{
+			Uniforms: map[string]any{
+				"Center": []float32{
+					float32(screenWidth) / 2,
+					float32(screenHeight) / 2,
+				},
+				"Time": time.Now().Sub(g.startTime).Seconds(),
+			},
+		},
+	)
+}
+
+// Update does nothing here
+func (g *Game) Update() error {
+	return nil
+}
+
+// Layout returns a fixed width/height
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	shaderFile, err := os.ReadFile("shader.kage")
+	if err != nil {
+		log.Panicf("Failed to read shader file: %v", err)
+	}
+
+	shader, err := ebiten.NewShader(shaderFile)
+	if err != nil {
+		log.Panicf("Failed to create shader: %v", err)
+	}
+
+	game = &Game{
+		shader:    shader,
+		startTime: time.Now(),
+	}
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
